cea2dbdb2ccacbdd5ecc1bb93e7238d491aac374
Apispeace <nhavis69@gmail.com>
2021-03-18 10:17:30 +0700

    initial commit

---
+++ app/Http/Controllers/LandingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class LandingController extends Controller
+{
+    public function index()
+    {
+
+       $products = Product::take(3)->get();
+        return view('welcome', compact('products'));
+    }
+}
